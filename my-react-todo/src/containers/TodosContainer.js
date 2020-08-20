23e60f79e9f6541d722c9b3dc3cd2248480949a5
u101004132 <dzag17188@gmail.com>
2020-08-20 13:45:10 -0400

    sprint 4

--- my-react-todo/src/containers/TodosContainer.js
+++ my-react-todo/src/containers/TodosContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CreateTodoForm from '../components/CreateTodoForm';
 import TodoModel from '../models/Todo';
 import Todos from '../components/Todos';
 
@@ -22,9 +23,25 @@ class TodosContainer extends Component {
         });
     };
 
-    render() {
+    createTodo = (todo) => {
+        let newTodo = {
+            body: todo,
+            completed: false,
+        };
+
+        TodoModel.create(newTodo).then((res) => {
+            let todos = this.state.todos;
+            todos.push(res.data);
+            this.setState({ todos: todos });
+        });
+    };
+
+    rrender() {
         return (
-            <div className="todosComponent">
+            <div className="todosContainer">
+                <CreateTodoForm
+                    createTodo={this.createTodo} />
+
                 <Todos
                     todos={this.state.todos} />
             </div>
